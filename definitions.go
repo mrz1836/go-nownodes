550b36e64a3044998527bc0fb2607d25851d9855
mrz1836 <mrz1818@gmail.com>
2022-01-31 16:38:29 -0500

    Bump version

--- definitions.go
+++ definitions.go
@@ -2,7 +2,7 @@ package nownodes
 
 const (
 	// version is the current version
-	version = "v0.0.3"
+	version = "v0.0.4"
 
 	// defaultUserAgent is the default user agent for all requests
 	defaultUserAgent string = "go-nownodes: " + version
